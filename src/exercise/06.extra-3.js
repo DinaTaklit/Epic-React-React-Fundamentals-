16a5528bc7e7e22dc473df075f3be9e127158595
DinaTaklit <ez_taklit@esi.dz>
2021-12-06 16:45:33 +0100

    feat:01. Basic JavaScript-rendered Hello World: 06: Forms:extra3: Control the input value

---
+++ src/exercise/06.extra-3.js
@@ -0,0 +1,58 @@
+// Basic Forms
+// http://localhost:3000/isolated/exercise/06.js
+
+import * as React from 'react'
+
+function UsernameForm({onSubmitUsername}) {
+  // 🐨 add a submit event handler here (`handleSubmit`).
+  // 💰 Make sure to accept the `event` as an argument and call
+  // `event.preventDefault()` to prevent the default behavior of form submit
+  // events (which refreshes the page).
+  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
+  //
+  // 🐨 get the value from the username input (using whichever method
+  // you prefer from the options mentioned in the instructions)
+  // 💰 For example: event.target.elements[0].value
+  // 🐨 Call `onSubmitUsername` with the value of the input
+
+  // 🐨 add the onSubmit handler to the <form> below
+
+  // 🐨 make sure to associate the label to the input.
+  // to do so, set the value of 'htmlFor' prop of the label to the id of input
+
+  const [username, setUsername] = React.useState(null)
+  const userNameRef = React.useRef(' type your usename here')
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    onSubmitUsername(userNameRef.current.value)
+  }
+
+  function handleChange(event) {
+    const {value} = event.target
+    setUsername(value.toLowerCase())
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          ref={userNameRef}
+          value={username}
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+function App() {
+  const onSubmitUsername = username => alert(`You entered: ${username}`)
+  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+}
+
+export default App

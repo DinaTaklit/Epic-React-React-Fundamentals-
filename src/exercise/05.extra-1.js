f46515c877da31ac6cc6606d2c419b85f94e9bd9
DinaTaklit <ez_taklit@esi.dz>
2021-12-06 15:04:23 +0100

    feat:01. Basic JavaScript-rendered Hello World: 05: Styling extra1: Create a custom component

---
+++ src/exercise/05.extra-1.js
@@ -0,0 +1,41 @@
+// Styling
+// http://localhost:3000/isolated/exercise/05.js
+
+import * as React from 'react'
+import '../box-styles.css'
+
+// 💰 Use the className for the size and style (backgroundColor, fontStyle) for the color and the font style
+// 💰 each of the elements should also have the "box" className applied
+
+// 🐨 add a className prop to each of these and apply the correct class names
+// 💰 Here are the available class names: box, box--large, box--medium, box--small
+
+// 🐨 add a style prop to each of them as well so their background color
+// matches what the text says it should be as well as `fontStyle: 'italic'`
+function Box({style, className = '', ...otherProps}) {
+  return (
+    <div
+      className={`box ${className}`}
+      style={{fontStyle: 'italic', ...style}}
+      {...otherProps}
+    />
+  )
+}
+function App() {
+  return (
+    <div>
+      <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box className="box--medium" style={{backgroundColor: 'pink'}}>
+        medium pink box
+      </Box>
+      <Box className="box--large" style={{backgroundColor: 'orange'}}>
+        large orange box
+      </Box>
+      <Box>sizeless box</Box>
+    </div>
+  )
+}
+
+export default App
